8f364c51904dc30f09ff6a3fa79df23dcee5d714
Ingvard <edvin.rodhe@gmail.com>
2026-08-29 21:31:54 +0200

    made code more consise

--- app/Main.hs
+++ app/Main.hs
@@ -9,36 +9,46 @@ import Data.Maybe
 donothing :: IO ()
 donothing = return ()
 
-removeLineMod :: String -> String
-removeLineMod line
+-- make it more *lispy*
+car :: (b,a) -> b
+cdr :: (a, b) -> b
+t :: Bool
+nil :: Bool
+car (a,_) = a
+cdr (_,a) = a
+t = True
+nil = False
+
+rlm :: String -> String
+rlm line
   | "\\head{3}" `isPrefixOf` line = fromMaybe line (stripPrefix "\\head{3}" line)
   | "\\head{2}" `isPrefixOf` line = fromMaybe line (stripPrefix "\\head{2}" line)
   | "\\head{1}" `isPrefixOf` line = fromMaybe line (stripPrefix "\\head{1}" line)
   | otherwise = line
 
-fndBckSlshPos :: String -> Maybe Int
-fndBckSlshPos line = elemIndex '\\' line
+fb :: String -> Maybe Int
+fb = (elemIndex '\\')
 
-checkIfthinginlist :: Eq t => t -> [t] -> Bool
-checkIfthinginlist thing list
-  | list == [] = False
+cil :: Eq t => t -> [t] -> Bool
+cil thing list
+  | list == [] = nil
   | otherwise =
     if head list == thing
-      then True
+      then t
     else let newlist = drop 1 list
-    in checkIfthinginlist thing newlist
+    in cil thing newlist
 
-checkwherethingisinlist :: (Eq t1, Num t2) =>
+cwl :: (Eq t1, Num t2) =>
                            t1 -> [t1] -> t2 -> t2
-checkwherethingisinlist thing list itnumber =
-  if head list == thing
-    then itnumber
-  else let newlist = drop 1 list
-           newitnum = itnumber + 1
-       in checkwherethingisinlist thing newlist newitnum
+cwl = \s l i ->
+  if head l == s
+    then i
+  else let nl = drop 1 l
+           ni = i + 1
+       in cwl s nl ni
 
-findEndOfCommand :: String -> Int -> Int
-findEndOfCommand line bckpos =
+fec :: String -> Int -> Int
+fec line bckpos =
   let afterBackslash = drop (bckpos + 1) line
       cmdName = takeWhile (/= '{') afterBackslash
       openBracePos = bckpos + 1 + length cmdName
@@ -46,17 +56,17 @@ findEndOfCommand line bckpos =
       contentLen = length (takeWhile (/= '}') afterOpen)
   in openBracePos + 1 + contentLen
 
-specLink :: [Char] -> [Char] -> [Char]
-specLink content rest =
+
+specCaseTwoArgs content rest =
   let secContent = takeWhile (/= '}') (drop 1 rest)
-  in "<a href=\"" ++ secContent ++ "\">" ++ content ++ "</a>"
+  in (content, secContent)
 
 
-procLine :: String -> String
-procLine line
+p :: String -> String
+p line
   | line == "" = ""
   | otherwise =
-    case fndBckSlshPos line of
+    case fb line of
       Nothing -> line
       
       Just pos ->
@@ -65,53 +75,59 @@ procLine line
             openBracePos = pos + 1 + length cmdName
             afterOpen = drop (openBracePos + 1) line
             content = takeWhile (/= '}') afterOpen
-            endPos = findEndOfCommand line pos
+            endPos = fec line pos
             rest = drop (endPos + 1) line
             before = take pos line
         in if cmdName == "bold"
-              then before ++ "<b>" ++ content ++ "</b>" ++ procLine rest
+              then before ++ "<b>" ++ content ++ "</b>" ++ p rest
            else if cmdName == "italic"
-              then before ++ "<i>" ++ content ++ "</i>" ++ procLine rest
+              then before ++ "<i>" ++ content ++ "</i>" ++ p rest
+           else if cmdName == "break"
+              then before ++ "<br>" ++ p rest
            else if cmdName == "link"
-              then before ++ (specLink content rest) ++ procLine (drop 1 (dropWhile (/= '}') rest))
+              then let ccc = specCaseTwoArgs content rest
+              in before ++ "<a href=\"" ++ (cdr ccc) ++ "\">" ++ (car ccc) ++ "</a>" ++ p (drop 1 (dropWhile (/= '}') rest))
            else if cmdName == "boldit"
-              then before ++ "<b><i>" ++ content ++ "</i></b>" ++ procLine rest
+              then before ++ "<b><i>" ++ content ++ "</i></b>" ++ p rest
            else if "head" `isPrefixOf` cmdName
               then let level = drop 4 cmdName
-                    in before ++ "<h" ++ level ++ ">" ++ content ++ "</h" ++ level ++ ">" ++ procLine rest
-           else before ++ "\\" ++ cmdName ++ "{" ++ content ++ "}" ++ procLine rest
+                    in before ++ "<h" ++ level ++ ">" ++ content ++ "</h" ++ level ++ ">" ++ p rest
+           else before ++ "\\" ++ cmdName ++ "{" ++ content ++ "}" ++ p rest
 
-wrapLine :: String -> String
-wrapLine line
-  | line == "" = ""
-  | otherwise =
-    let pMark = checkLineForMod line
-        cont = removeLineMod line
-        processed = procLine cont
-    in "<" ++ pMark ++ ">" ++ processed ++ "</" ++ pMark ++ ">"
+w :: String -> String
+w =
+    let pm = clm
+        c = rlm
+        pr = p . c
+    in \l -> "<" ++ pm l ++ ">" ++ pr l ++ "</" ++ pm l ++ ">"
 
-failing :: [Char] -> IO b
-failing reason = do
-  putStrLn ("Failed: " ++ reason)
+fl :: [Char] -> IO b
+fl r = do
+  putStrLn ("Failed: " ++ r)
   exitFailure
 
-checkLineForMod :: [Char] -> String
-checkLineForMod line
+clm :: [Char] -> String
+clm line
   | "\\head{3}" `isPrefixOf` line = "h3"
   | "\\head{2}" `isPrefixOf` line = "h2"
   | "\\head{1}" `isPrefixOf` line = "h1"
   | otherwise = "p"
 
+re = \l ->
+  if l == "<p></p>" then "<br>" else l
+
+
 main :: IO ()
 main = do
-  args <- getArgs
-  if length args == 0
-    then failing "no filename"
+  a <- getArgs
+  if length a == 0
+    then fl "no filename"
     else donothing
-  let outname = if checkIfthinginlist "-o" args then args !! ((checkwherethingisinlist "-o" args 0) + 1) else "out.html"
-  file <- openFile (args !! ((length args) - 1)) ReadMode
-  filecont <- hGetContents file
-  let linescontlist = lines filecont
-  let htmlLines = map wrapLine linescontlist
-  let htmlCont = unlines htmlLines
-  writeFile outname htmlCont
+  let on = if cil "-o" a then a !! ((cwl "-o" a 0) + 1) else "out.html"
+  f <- openFile (a !! ((length a) - 1)) ReadMode
+  fc <- hGetContents f
+  let lcl = lines fc
+  let hl = map w lcl
+  let hl2 = map re hl
+  let hc = unlines hl2
+  writeFile on hc
