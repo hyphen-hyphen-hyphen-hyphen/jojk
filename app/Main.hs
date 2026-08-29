52ceb2304fe70099c79702a605d3d0855d0a4b6e
Ingvard <edvin.rodhe@gmail.com>
2026-08-29 17:30:33 +0200

    Added link suppoooort!!!!!!

--- app/Main.hs
+++ app/Main.hs
@@ -27,36 +27,47 @@ findEndOfCommand line bckpos =
       contentLen = length (takeWhile (/= '}') afterOpen)
   in openBracePos + 1 + contentLen
 
-procLine :: String -> String
-procLine line =
-  case fndBckSlshPos line of
-    Nothing -> line
-    
-    Just pos ->
-      let afterBackslash = drop (pos + 1) line
-          cmdName = takeWhile (/= '{') afterBackslash
-          openBracePos = pos + 1 + length cmdName
-          afterOpen = drop (openBracePos + 1) line
-          content = takeWhile (/= '}') afterOpen
-          endPos = findEndOfCommand line pos
-          rest = drop (endPos + 1) line
-          before = take pos line
-      in if cmdName == "bold"
-            then before ++ "<b>" ++ content ++ "</b>" ++ procLine rest
-         else if cmdName == "ital"
-            then before ++ "<i>" ++ content ++ "</i>" ++ procLine rest
-         else if cmdName == "boldit"
-            then before ++ "<b><i>" ++ content ++ "</i></b>" ++ procLine rest
-         else if "head" `isPrefixOf` cmdName
-            then let level = drop 4 cmdName
-                  in before ++ "<h" ++ level ++ ">" ++ content ++ "</h" ++ level ++ ">" ++ procLine rest
-         else before ++ "\\" ++ cmdName ++ "{" ++ content ++ "}" ++ procLine rest
+specLink content rest =
+  let secContent = takeWhile (/= '}') (drop 1 rest)
+  in "<a href=\"" ++ secContent ++ "\">" ++ content ++ "</a>"
 
-wrapLine line =
-  let pMark = checkLineForMod line
-      cont = removeLineMod line
-      processed = procLine cont
-  in "<" ++ pMark ++ ">" ++ processed ++ "</" ++ pMark ++ ">"
+
+procLine :: String -> String
+procLine line
+  | line == "" = ""
+  | otherwise =
+    case fndBckSlshPos line of
+      Nothing -> line
+      
+      Just pos ->
+        let afterBackslash = drop (pos + 1) line
+            cmdName = takeWhile (/= '{') afterBackslash
+            openBracePos = pos + 1 + length cmdName
+            afterOpen = drop (openBracePos + 1) line
+            content = takeWhile (/= '}') afterOpen
+            endPos = findEndOfCommand line pos
+            rest = drop (endPos + 1) line
+            before = take pos line
+        in if cmdName == "bold"
+              then before ++ "<b>" ++ content ++ "</b>" ++ procLine rest
+           else if cmdName == "italic"
+              then before ++ "<i>" ++ content ++ "</i>" ++ procLine rest
+           else if cmdName == "link"
+              then before ++ (specLink content rest) ++ procLine (drop 1 (dropWhile (/= '}') rest))
+           else if cmdName == "boldit"
+              then before ++ "<b><i>" ++ content ++ "</i></b>" ++ procLine rest
+           else if "head" `isPrefixOf` cmdName
+              then let level = drop 4 cmdName
+                    in before ++ "<h" ++ level ++ ">" ++ content ++ "</h" ++ level ++ ">" ++ procLine rest
+           else before ++ "\\" ++ cmdName ++ "{" ++ content ++ "}" ++ procLine rest
+
+wrapLine line
+  | line == "" = ""
+  | otherwise =
+    let pMark = checkLineForMod line
+        cont = removeLineMod line
+        processed = procLine cont
+    in "<" ++ pMark ++ ">" ++ processed ++ "</" ++ pMark ++ ">"
 
 failing reason = do
   putStrLn ("Failed: " ++ reason)
