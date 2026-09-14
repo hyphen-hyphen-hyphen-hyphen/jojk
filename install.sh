712142b1641c0597e077c0214a2f4c1b818d86c7
Ingvard <edvin.rodhe@gmail.com>
2026-09-14 23:53:22 +0200

    update install script, more efficient

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
-git clone https://github.com/hyphen-hyphen-hyphen-hyphen/jojk.git
-cd jojk/app/
+curl -Lf https://raw.github.com/hyphen-hyphen-hyphen-hyphen/jojk/blob/trunk/app/Main.hs > Main.hs
 ghc Main.hs
 sudo cp Main /usr/bin/jojk
-cd ../../
-rm -rf jojk
+rm Main Main.o Main.hi Main.hs
